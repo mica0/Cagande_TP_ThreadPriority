60bd77c7834c98cf575cc9cb70beebeec54f97aa
Mica0 <cagandemicaela123@gmail.com>
2023-10-27 18:12:13 +0800

    SUPPER FINAL THREAD-PRIORITY

--- Cagande_TP_ThreadPriority/MyThreadClass.cs
+++ Cagande_TP_ThreadPriority/MyThreadClass.cs
@@ -11,7 +11,7 @@ namespace Cagande_TP_ThreadPriority
     {
         public  static void Thread1()
         { 
-            for (int loopCount = 0; loopCount < 2; loopCount++)
+            for (int loopCount = 0; loopCount < 3; loopCount++)
             {
                 Thread thread = Thread.CurrentThread;
                 Console.WriteLine("Name of Thread: " + thread.Name + " = " + loopCount);
